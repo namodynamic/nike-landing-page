41c4a23e55e770ce79ef083950d85d7f4a8502cb
namodynamic <nnamdi4u09@gmail.com>
2023-08-21 15:40:30 +0100

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Subscribe from "./Subscribe";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+
+export {
+    Hero,
+    PopularProducts,
+    Subscribe,
+    Services,
+    SpecialOffers,
+    SuperQuality,
+    CustomerReviews,
+    Footer,
+}
